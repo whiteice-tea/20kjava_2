343efd03780a1e7edb8661c35e94a85e53d09105
whiteice-tea <whiteicetea4@gmail.com>
2026-03-08 21:32:33 +0800

    new

---
+++ src/main/java/Enumeration/Enumte.java
@@ -0,0 +1,8 @@
+package Enumeration;
+
+/**
+ * @author whiteicetea
+ * @version 1.0.0
+ */
+public class Enumte {
+}
--- src/main/java/Enumeration/test3.java
+++ src/main/java/Enumeration/test3.java
@@ -6,7 +6,22 @@ package Enumeration;
  */
 public class test3 {
     public static void main(String[] args) {
+
 //        System.out.println(Season2.AUTUMN);
+        Season2 spring =Season2.SPRING;
+        Season2 autumn = Season2.AUTUMN;
+        System.out.println(autumn.name());
+        //ordinal 输出的是该枚举对象的次序/编号   从零开始
+        System.out.println(spring.ordinal());
+        System.out.println(autumn.ordinal());
+        //values方法，返回Season2[]
+
+        Season2[] values = Season2.values();
+        for(Season2 s:values){
+            System.out.println(s.name());
+        }
+
+
     }
 }
 
