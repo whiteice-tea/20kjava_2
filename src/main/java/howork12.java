4ab83cbe193978a326be2ca6580ea4669e100aea
whiteice-tea <whiteicetea4@gmail.com>
2026-03-11 14:18:27 +0800

    new

--- src/main/java/howork12.java
+++ src/main/java/howork12.java
@@ -4,11 +4,33 @@
  */
 public class howork12 {
     public static void main(String[] args) {
-
+        color c=color.BLACK;
+        switch(c){
+            case RED:
+                System.out.println("匹配到红色");
+                c.show();
+                break;
+            case BLUE:
+                System.out.println("匹配到蓝色");
+                c.show();
+                break;
+            case BLACK:
+                System.out.println("匹配到黑色");
+                c.show();
+                break;
+            case YELLOW:
+                System.out.println("匹配到黄色");
+                c.show();
+                break;
+            case GREEN:
+                System.out.println("匹配到绿色");
+                c.show();
+                break;
+        }
     }
 }
 
-enum color{
+enum color implements showcolor{
     RED(255,0,0),
     BLUE(0,0,255),
     GREEN(0,0,0),
@@ -16,12 +38,25 @@ enum color{
     BLACK(0,0,0);
 
     private int redValue;
-    private int blueValue;
     private int greenValue;
+    private int blueValue;
 
-    color(int redValue, int blueValue, int greenValue) {
+    color(int redValue, int greenValue, int blueValue) {
         this.redValue = redValue;
         this.blueValue = blueValue;
         this.greenValue = greenValue;
     }
+
+    @Override
+    public void show() {
+        System.out.println("redValue=" + redValue +
+                ", greenValue=" + greenValue +
+                ", blueValue=" + blueValue);
+    }
+
+}
+
+
+interface showcolor{
+    void show();
 }
